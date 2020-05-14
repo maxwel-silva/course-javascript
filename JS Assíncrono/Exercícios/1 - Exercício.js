87313abece50f1dd7448ddb7d9f5f7e9e1a75702
maxwel-silva <maxwel.santos4@etec.sp.gov.br>
2020-05-14 10:00:09 -0300

    first commit

--- JS Assíncrono/Exercícios/1 - Exercício.js
+++ JS Assíncrono/Exercícios/1 - Exercício.js
@@ -1,11 +1,6 @@
 'use strict'
 
-/*
-
-Crie uma função que recebe a idade de um usuário e retorna uma Promise que depois de 2
-segundos retornará se usuário é maior ou não que 18 anos
-
-*/
+// Crie uma função que recebe a idade de um usuário e retorna uma Promise que depois de 2 segundos retornará se usuário é maior ou não que 18 anos
 
 function verificar(idade) {
   return new Promise(function (resolve, reject) {
